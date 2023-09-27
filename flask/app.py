526a2fedbaa8265d8c99f35b95f053e72caf998a
Alon <alonmarga@gmail.com>
2023-09-27 13:28:18 +0300

    flask and grafana are connected

--- flask/app.py
+++ flask/app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/dash')
 def index():
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
