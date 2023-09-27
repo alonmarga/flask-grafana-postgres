f1aa1cbab87ddb3d6452ef69c2fbf165325283be
Alon <alonmarga@gmail.com>
2023-09-27 12:42:33 +0300

    embadding grafana to flask

--- flask/app.py
+++ flask/app.py
@@ -2,16 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/dash')
-def display_dashboard():
-    # Paste the copied iframe code here
-    #grafana_iframe = '<paste your iframe code here>'
-    #return render_template('dashboard.html', grafana_iframe=grafana_iframe)
+@app.route('/')
+def index():
+    return render_template('dashboard.html')
 
-    dummy_iframe_code = '<iframe width="800" height="600" src="https://www.example.com" frameborder="0" allowfullscreen></iframe>'
-    return render_template('dashboard.html', grafana_iframe=dummy_iframe_code)
-
-
-    
 if __name__ == '__main__':
     app.run(debug=True)
